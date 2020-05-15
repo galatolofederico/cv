951ff65ecfe38f287adadb866af6d7b1a93f372e
Federico Galatolo <galatolo.federico@gmail.com>
2020-05-15 19:34:14 +0200

    sidebar nice

--- generate.py
+++ generate.py
@@ -1,20 +1,57 @@
 import json
 
-mejson = json.load(open("me.json/me.json", "r"))
-template = open("template.tex", "r").read()
+def getPlatform(mejson, name, raw=False):
+    for profile in mejson["digitalidentity"]["profiles"]:
+        if profile["platform"] == name:
+            if raw:
+                return profile["link"]
+            else:
+                return profile["link"].split("/")[-1]
+    return "Not Found"
 
+
+def getBibTeX(pub):
+    if pub["type"] == "conference":
+        return """
+
+        """
+    if pub["type"] == "journal":
+        return """
+
+        """
+
+
+mejson = json.load(open("me.json/me.json", "r"))
 
 placeholders = {
     "@firstname": mejson["anagraphic"]["fullname"]["first"]+" "+mejson["anagraphic"]["fullname"]["middle"][0]+". ",
     "@lastname": mejson["anagraphic"]["fullname"]["last"],
     "@qualification": mejson["anagraphic"]["qualifications"][1],
-    "@city": mejson["anagraphic"]["address"]["city"]+" "+mejson["anagraphic"]["address"]["cap"],
+    "@city": mejson["anagraphic"]["address"]["city"]+" ("+mejson["anagraphic"]["address"]["cap"]+")",
     "@street": mejson["anagraphic"]["address"]["street"],
-    "@building": mejson["anagraphic"]["address"]["building"]
+    "@building": mejson["anagraphic"]["address"]["building"],
+    "@website": mejson["digitalidentity"]["website"].split("://")[1],
+    "@url_website": mejson["digitalidentity"]["website"],
+    "@phone": mejson["digitalidentity"]["telephone"],
+    "@url_phone": "tel:"+mejson["digitalidentity"]["telephone"],
+    "@email": mejson["digitalidentity"]["email"].replace("@", " @"),
+    "@url_email": "mailto:"+mejson["digitalidentity"]["email"].replace("@", " @"),
+    "@telegram": getPlatform(mejson, "Telegram"),
+    "@url_telegram": getPlatform(mejson, "Telegram", True),
+    "@github": getPlatform(mejson, "GitHub"),
+    "@url_github": getPlatform(mejson, "GitHub", True),
+    
 }
 
+
+template = open("template.tex", "r").read()
+cv = open("cv.tex", "w")
 for key, value in placeholders.items():
     template = template.replace(key, value)
+cv.write(template)
+cv.close()
 
-
-open("cv.tex", "w").write(template)
+bibliography = open("bibliography.bib", "w")
+for pub in mejson["publications"]:
+    bibliography.write(getBibTeX(pub))
+bibliography.close()
